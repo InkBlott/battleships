523f6160ebd0cd5ef61294edf1391c6ff0adc903
Ink <bmantas@tutanota.com>
2021-03-09 21:17:52 +0200

    draggable margin fix

--- src/components/GabeInfoTable.js
+++ src/components/GabeInfoTable.js
@@ -4,12 +4,12 @@ import '../styles/Styles.css';
 export default function GameInfoTable({placer ,shipsSet, isV, rotate, reset }) {
 
     function drag(e) {
-        e.dataTransfer.setData('text/plain', e.target.id);
+        e.dataTransfer.setData('text/plain', e.target.parentElement.id);
     }
 
     function getClass() {
         if (isV) {
-            return 'col'
+            return ''
         } else return 'row'
 
     }
@@ -20,8 +20,8 @@ export default function GameInfoTable({placer ,shipsSet, isV, rotate, reset }) {
             <div className='row subT'>Place your ships:</div>
 
             <div className='row'>
-                <div id='ShipPlacer0.4' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer0.4' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
@@ -29,57 +29,57 @@ export default function GameInfoTable({placer ,shipsSet, isV, rotate, reset }) {
                     </div>
 
                 </div>
-                <div id='ShipPlacer1.3' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer1.3' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
 
                 </div>
-                <div id='ShipPlacer2.3' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer2.3' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
 
-                <div id='ShipPlacer3.2' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer3.2' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer4.2' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer4.2' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer5.2' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer5.2' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer6.1' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer6.1' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer7.1' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer7.1' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer8.1' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer8.1' className='col placerContainer'>
+                    <div draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
-                <div id='ShipPlacer9.1' className='col placerContainer' draggable='true' onDragStart={(e) => drag(e)}>
-                    <div className={getClass() + ' blr'}>
+                <div id='ShipPlacer9.1' className='col placerContainer'>
+                    <div  draggable='true' onDragStart={(e) => drag(e)} className={getClass() + ' blr'}>
                         <div className={'col boardButton ship placer noHover'}></div>
                     </div>
                 </div>
